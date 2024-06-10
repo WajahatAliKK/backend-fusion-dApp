731320e57d2a280eb2a265f669741599f0581d0d
ehtasham-farid <164297277+ehtasham-farid@users.noreply.github.com>
2024-06-10 16:40:09 +0500

    Update routes.js (imports that transferTokens)

--- routes/routes.js
+++ routes/routes.js
@@ -3,7 +3,7 @@
 import express from 'express';
 import { createWallet, getAllWallets, getWalletByAddress ,getDecryptedKey } from '../controllers/walletController.js';
 import { getQuickBuySettings, getQuickSellSettings, setQuickBuySettings, setQuickSellSettings } from '../controllers/settingsController.js';
-import { swapTokensExactOut, swapTokensExactIn } from '../controllers/swapController.js';
+import { swapTokensExactOut, swapTokensExactIn,transferTokens } from '../controllers/swapController.js';
 const router = express.Router();
 
 router.get('/createWalletAndSaveToMongoDB/:publicAddress', createWallet);
