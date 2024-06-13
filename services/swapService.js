70567d60245091ebad79d34cc271d72e649584a8
ehtasham-farid <164297277+ehtasham-farid@users.noreply.github.com>
2024-06-13 16:04:28 +0500

    Update swapService.js( export getItemsBySource)

--- services/swapService.js
+++ services/swapService.js
@@ -1,10 +1,11 @@
 import { swapTokens, swapInstructions, transferSOL, withdraw } from './swap.js';
-import { getRecentData } from '../services/newPairs.js' ;
+import { getRecentData , getItemsBySource} from '../services/newPairs.js' ;
 // import{decryptPrivateKey } from '../utils/encryption.js';
 export {
     swapTokens,
     swapInstructions,
     transferSOL,
     withdraw,
-    getRecentData
+    getRecentData,
+    getItemsBySource
 };
